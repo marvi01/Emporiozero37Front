c2480cbef487a9616ffd1273d42769489f7a17b6
wt2k <miguelfernandesalves09@gmail.com>
2020-06-25 18:53:19 -0300

    Revert "aaa"
    This reverts commit 236c8da0dd479f0778e520d62fb82efde87d3bd8.

--- src/App.js
+++ src/App.js
@@ -3,13 +3,11 @@ import './App.css';
 import Produto from './Component/Produto/Produto';
 import Contato from './Component/Contato/Contato';
 import Destaque from './Component/Destaque/Destaque';
-import MenuSuperior from './Component/MenuSuperior/MenuSuperior';
 function App() {
   return (
     <div >
 
       <Contato></Contato>
-      <MenuSuperior></MenuSuperior>
       <Destaque></Destaque>
       <Produto></Produto>
 
