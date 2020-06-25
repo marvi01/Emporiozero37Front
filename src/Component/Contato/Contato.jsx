e4ea86a446c33e50e5e1ab1b2ced98e0aa85a194
marvi01 <marcosvini10987@gmail.com>
2020-06-24 23:15:54 -0300

    Contatos

--- src/Component/Contato/Contato.jsx
+++ src/Component/Contato/Contato.jsx
@@ -10,9 +10,15 @@ class header extends Component {
 
     render() {
         return (
-            <div className="Contato">
+            <div>
+                <div className="Contato">
+                    <div className="tam">
+                        <a href="https://www.instagram.com/emporiozero37/?hl=pt-br">
+                            <img className="instagram " src={insta} alt="" />
+                        </a>
+                    </div>
+                </div>
 
-                <a  href="https://www.instagram.com/emporiozero37/?hl=pt-br"><img className="instagram" src={insta} alt=""/></a>
             </div>
         );
     }
--- src/Component/Produto/Produto.jsx
+++ src/Component/Produto/Produto.jsx
@@ -5,78 +5,78 @@ import './Produto.css';
 
 class Produto extends Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            "data":[{
-                "id":0,
-            "nomeprod":"",
-            "descricao":"",
-            "foto":"",
-            "preco":0,
-            "teor":0,
-            "ml":0,
-            "quantidade":0,
-            "categoria_id":0,
-            "created_at":"",
-            "updated_at":""}],
-            "status":false
-        };
+  constructor(props) {
+    super(props);
+    this.state = {
+      "data": [{
+        "id": 0,
+        "nomeprod": "",
+        "descricao": "",
+        "foto": "",
+        "preco": 0,
+        "teor": 0,
+        "ml": 0,
+        "quantidade": 0,
+        "categoria_id": 0,
+        "created_at": "",
+        "updated_at": ""
+      }],
+      "status": false
     };
-    componentDidMount() {
-        fetch("https://anorosa.com.br/Emporio037/api/produto/list")
-        .then(data => data.json().then(data => this.setState({ data })))
-        .catch(erro=>this.setState(erro));
-        
+  };
+  componentDidMount() {
+    fetch("https://anorosa.com.br/Emporio037/api/produto/list")
+      .then(data => data.json().then(data => this.setState({ data })))
+      .catch(erro => this.setState(erro));
+
+  }
+  exibeErro() {
+    const { erro } = this.state;
+
+    if (erro) {
+      return (
+        <div className="alert alert-danger" role="alert">
+          Erro de conexão com o servidor
+        </div>
+      );
     }
-    exibeErro() {
-        const { erro } = this.state;
-    
-        if (erro) {
-          return (
-            <div className="alert alert-danger" role="alert">
-              Erro de conexão com o servidor
-            </div>
-          );
-        }
-      }
-      exibeProduto()
-      {
-        const { data } = this.state.data;
-        if (data && data.length){
-            const Prod = data.map((item,indice)=>( 
-                 
-              <div  key = {indice}  className="card tamanho group" >
-              <img className="card-img-top foto" src={`https://anorosa.com.br/Emporio037/storage/${item.foto}`}   />
-              <div text align= "center " className="card-body desc">
-                  <h5 className="card-title">{item.nomeprod}</h5>
+  }
+  exibeProduto() {
+    const { data } = this.state.data;
+    if (data && data.length) {
+      const Prod = data.map((item, indice) => (
+
+        <div key={indice} className="card tamanho group" >
+          <img className="card-img-top foto" src={`https://anorosa.com.br/Emporio037/storage/${item.foto}`} />
+          <div text align="center " className="card-body desc">
+            <h5 className="card-title">{item.nomeprod}</h5>
             <p>{item.teor}</p>
 
-        
-                  
-              </div>
-          </div>
-            )   
-             )
-             return Prod;
-              } else{
-                return(
-                <div className="alert alert-light">
-               <p>Nenhum produto encontrado :(</p>
-              </div>)
-             }
 
-       }
-       //
-    render() {
-        return (
-            
-            <div className =" container">  
-                 
-            <div>{this.exibeErro() || this.exibeProduto()}</div>
-            </div>
-        );
-    };
+
+          </div>
+        </div>
+      )
+      )
+      return Prod;
+    } else {
+      return (
+        <div className="alert alert-light">
+          <p>Nenhum produto encontrado :(</p>
+        </div>)
+    }
+
+  }
+  //
+  render() {
+    return (
+
+      <div className=" container">
+
+        <div>{this.exibeErro() || this.exibeProduto()}</div>
+      </div>
+    );
+  };
 
 
 };
