c8d40a8997abc704cfcb1e66ad718dd3afa3b9d3
marvi01 <marcosvini10987@gmail.com>
2020-06-29 16:29:52 -0300

    Categoria Especifica Implementada

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Contato from './Component/Contato/Contato';
 import ProdutoEspc from './Component/ProdutoEspc/ProdutoEspc';
 import MenuPrincial from './Component/MenuPrincipal/MenuPrincipal';
 import MenuSuperior from './Component/MenuSuperior/MenuSuperior';
+import CategoriaEspc from './Component/CategoriaEspc/CategoriaEspc';
 import {Switch, Route} from 'react-router-dom';
 import RotasMenuSup from './Component/MenuSuperior/RotasMenuSup';
 function App() {
@@ -15,6 +16,7 @@ function App() {
       <Switch>
       <Route path="/" exact  component={RotasMenuSup}/>
       <Route path = "/Produto/:id" exact component = {ProdutoEspc}/>
+      <Route path ="/Categoria/:id" exact component={CategoriaEspc}/>
       
       </Switch>
     </div>
