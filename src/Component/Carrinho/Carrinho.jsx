17bf2cb402ff047e50b7d9921e91b7869d918c78
marvi01 <marcosvini10987@gmail.com>
2020-06-30 19:22:41 -0300

    Icones Adicionados

--- src/Component/Carrinho/Carrinho.jsx
+++ src/Component/Carrinho/Carrinho.jsx
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
+import 'font-awesome/css/font-awesome.min.css';
+
+
 
 class Carrinho extends Component {
     render() {
         return (
             <div>
                 
-                <div>
-              Carrinho
-               </div>                 
+                <i class="fab fa-accessible-icon"></i>         
             </div>
         );
     }
