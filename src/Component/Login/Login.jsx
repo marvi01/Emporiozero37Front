00b00d63905698d1aa307645512210a73031b9ed
marvi01 <marcosvini10987@gmail.com>
2020-06-30 13:31:34 -0300

    Login Cadastro INicio

--- src/Component/Login/Login.jsx
+++ src/Component/Login/Login.jsx
@@ -39,7 +39,7 @@ export default function Login(){
     }else{
         return (
             <div>
-                <p>Cadastro</p>
+                <p>HTLML DE CADASTRO DENTRO DESSA DIV</p>
             <button onClick={()=> setLogin(true)}>
                 troca
             </button>
