d27bf2b74313737650b0b26512468a135d480760
wt2k <miguelfernandesalves09@gmail.com>
2020-09-02 16:09:23 -0300

    aaaaaaa

--- src/Component/Destaque/Destaque.jsx
+++ src/Component/Destaque/Destaque.jsx
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import '../Destaque/Destaque.css';
-import destaque1  from "../../imagens/destaque1.webp";
-import destaque2  from "../../imagens/destaque2.webp";
-import destaque3  from "../../imagens/destaque3.webp";
+import destaque1 from "../../imagens/destaque1.webp";
+import destaque2 from "../../imagens/destaque2.webp";
+import destaque3 from "../../imagens/destaque3.webp";
 
 
 import { Slide } from 'react-slideshow-image';
@@ -10,72 +10,73 @@ class Destaque extends Component {
   constructor(props) {
     super(props);
     this.state = {
-        data: [{
-            "id": 0,
-            "fotodestaque": "",
-        }],
+      data: [{
+        "id": 0,
+        "fotodestaque": "",
+      }],
 
-        erro: null,
-        nulo: true,
-        estado: false,
-        status: false
+      erro: null,
+      nulo: true,
+      estado: false,
+      status: false
     };
   };
   async componentDidMount() {
     var response;
 
-        try {
-            response = await fetch(`https://anorosa.com.br/Emporio037/api/destaque/list`);
-        } catch (error) {
-            console.log(error);
-            this.setState({ error })
-        }
-        const json = await response.json();
+    try {
+      response = await fetch(`https://anorosa.com.br/Emporio037/api/destaque/list`);
+    } catch (error) {
+      console.log(error);
+      this.setState({ error })
+    }
+    const json = await response.json();
 
-        if (json.error === null || json.error === undefined || json.status === true) {
-            this.setState({ data: json.data, nulo: false });
-            console.log(json);
-        }
-        this.setState({ estado: true });
+    if (json.error === null || json.error === undefined || json.status === true) {
+      this.setState({ data: json.data, nulo: false });
+      console.log(json);
     }
-    exibirDestaque(){
-      if(this.state.error === null && this.state.status === true && this.state.nulo === false){
-        const Destaq = this.state.data.map((item, indice) => {
-          return(
-          <div className="each-slide" key={indice}>
-                  <div >
-                    <img src={`https://anorosa.com.br/Emporio037/storage/${item.fotodestaque}`} className="img-fluid" alt="..."></img>
-                  </div>
-          </div>
-          )
-        });
-        return Destaq;
-      }
-    }
-    render() {
-          const properties = {
-            duration: 5000,
-            transitionDuration: 500,
-            infinite: true,
-            indicators: true,
-            arrows: true,
-            pauseOnHover: true,
-          }
+    this.setState({ estado: true });
+  }
+  exibirDestaque() {
+    if (this.state.erro === null && this.state.estado === true && this.state.nulo === false) {
+      const Destaq = this.state.data.map((item, indice) => {
         return (
-            <div className="disan" >
-              <Slide {...properties}>
-                <div className="each-slide">
-                  <div >
-                    <img src={destaque1} className="img-fluid" alt="..."></img>
-                  </div>
-                </div>
-                {this.exibirDestaque()}
-                
-              </Slide>
-              
-            </div>
-        );
+          <div >
+            <img src={`https://anorosa.com.br/Emporio037/storage/${item.fotodestaque}`} className="img-fluid" alt="..."></img>
+          </div>
+
+        )
+      });
+      console.log(Destaq)
+      return Destaq;
     }
+  }
+  render() {
+    const properties = {
+      duration: 5000,
+      transitionDuration: 500,
+      infinite: true,
+      indicators: true,
+      arrows: true,
+      pauseOnHover: true,
+    }
+    const destaques = this.exibirDestaque();
+    return (
+      <div className="disan" >
+        <Slide {...properties}>
+          <div className="each-slide">
+            <div >
+              <img src={destaque1} className="img-fluid" alt="..."></img>
+            </div>
+          </div>
+          {destaques}
+
+        </Slide>
+
+      </div>
+    );
+  }
 
 }
 
